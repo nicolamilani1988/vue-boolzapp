466d0dd32b03934088711821c53bf3360f06b2bb
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-04-09 11:41:17 +0200

    scritta funzione per creare array di text

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function init(){
   
     new Vue ({
         el: "#app",
+
         data:{
+
             contacts: [
                 {
                     name: 'Michele',
@@ -26,72 +28,34 @@ function init(){
                         }
                     ],
                 },
-                {
-                    name: 'Fabio',
-                    avatar: '_2',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '20/03/2020 16:30:00',
-                            text: 'Ciao come stai?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '20/03/2020 16:30:55',
-                            text: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
-                        },
-                        {
-                            date: '20/03/2020 16:35:00',
-                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
-                        }
-                    ],
-                },
-                {
-                    name: 'Samuele',
-                    avatar: '_3',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '28/03/2020 10:10:40',
-                            text: 'La Marianna va in campagna',
-                            status: 'received'
-                        },
-                        {
-                            date: '28/03/2020 10:20:10',
-                            text: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '28/03/2020 16:15:22',
-                            text: 'Ah scusa!',
-                            status: 'received'
-                        }
-                    ],
-                },
-                {
-                    name: 'Luisa',
-                    avatar: '_4',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            text: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            text: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
-                        }
-                    ],
-                },
-            ];
+                //aggiungere altri utenti
+            ],
+
+            texts: []
+
         },
+
         methods:{
-        
+            getContactText: function(){
+                for(let i=0;i<this.contacts.length;i++){
+                    const contact = this.contacts[i];
+                    const messages = contact['messages'];
+                    const text = messages[0]['text'];
+                    console.log(text);
+                }
+            }
         },
+
+        mounted(){
+            const messages = this.contacts[0]['messages'];
+            for (let i = 0;i<messages.length;i++){
+                const message = messages[i];
+                const text = message['text'];
+                this.texts.push(text);
+            }
+            console.log(this.texts);
+        }
+
     })
 
 }
